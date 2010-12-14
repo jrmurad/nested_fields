b1d15581f53dcfe1d2bcec39695fdbda4e14dc46
Jason Murad <jason@thriess.com>
2010-12-14 16:49:23 -0500

    alphabetical order of locals and more specific class name for remove link

--- app/helpers/nested_fields_helper.rb
+++ app/helpers/nested_fields_helper.rb
@@ -6,13 +6,13 @@ module NestedFieldsHelper
   def nested_fields_for(f, association, nested_partial="#{association}/fields")
     add = add_nested_fields_path :parent_class => f.object.class, :association => association, :builder => f.class
 
-    render 'nested_fields/nested_fields', :association => association,
-                                          :add_url => add,
+    render 'nested_fields/nested_fields', :add_url => add,
+                                          :association => association,
                                           :f => f,
                                           :nested_partial => nested_partial
   end
 
   def link_to_remove_fields(f, text='Remove')
-    link_to text, '#', :class => :remove_fields
+    link_to text, '#', :class => :remove_nested_fields
   end
 end

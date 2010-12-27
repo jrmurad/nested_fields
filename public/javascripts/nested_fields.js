a53bf26e10e8008d2adf36077049c9ca0276dfc6
Jason Murad <jason@thriess.com>
2010-12-27 10:43:09 -0500

    handle positioning

--- public/javascripts/nested_fields.js
+++ public/javascripts/nested_fields.js
@@ -28,3 +28,21 @@ $('.nested_fields_has_one').live('change', function() {
   $(hidden_field).val($(hidden_field).val() == 'true' ? 'false' : 'true');
   $(this).closest('.nested_fieldset').find('.nested_one').toggle();
 });
+
+function reposition(nested_fieldset, pos_fld) {
+  $(nested_fieldset).find('> .nested_fields').each(function(i, nested_fields) {
+    $(nested_fields).find('input[id$="' + pos_fld + '"]').val(i);
+  });
+}
+
+$('.nested_fieldset').live('nested_fields_ready', function() {
+  var pos_fld = this.getAttribute('data-positioned-by');
+
+  if (pos_fld) {
+    reposition(this, pos_fld);
+
+    if (!$(this).hasClass('ui-sortable')) {
+      $(this).sortable({update: function() { reposition(this, pos_fld); }});
+    }
+  }
+});

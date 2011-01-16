230f575ca9554b518e9af4e4ab81dfa23ad06cf5
Jason Murad <jason@thriess.com>
2011-01-16 12:03:06 -0500

    nested_partial and legend options

--- app/helpers/nested_fields_helper.rb
+++ app/helpers/nested_fields_helper.rb
@@ -1,22 +1,19 @@
 module NestedFieldsHelper
-  def nested_fields_for(f, association, nested_partial=nil, options={})
+  def nested_fields_for(f, association, options={})
     reflection = f.object.class.reflect_on_association(association)
     is_has_one = reflection.macro == :has_one
-    nested_partial ||= "#{is_has_one ? association.to_s.pluralize : association}/fields"
+    options[:nested_partial] ||= "#{is_has_one ? association.to_s.pluralize : association}/fields"
 
     add = add_nested_fields_path :association => association,
                                  :builder => f.class,
-                                 :nested_partial => nested_partial,
+                                 :nested_partial => options[:nested_partial],
                                  :parent_class => f.object.class,
                                  :parent_form => f.object_name
 
 
     partial = "nested_fields/#{'singular_' if is_has_one}nested_fieldset"
 
-    locals = {:add_url => add,
-              :association => association,
-              :f => f,
-              :nested_partial => nested_partial}
+    locals = {:add_url => add, :association => association, :f => f}
 
     locals.merge!(options)
 

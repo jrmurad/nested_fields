1ad6c85b0b1323ec00cb81c7f97f1ab28e5f4ac0
Jason Murad <jason@thriess.com>
2011-01-25 20:33:20 -0500

    simplify and let testing work

--- lib/nested_fields.rb
+++ lib/nested_fields.rb
@@ -1,1 +1,11 @@
-require 'engine'
+module NestedFields
+  class Engine < Rails::Engine
+    initializer 'nested_fields.add_middleware' do |app|
+      app.middleware.use ActionDispatch::Static, "#{root}/public"
+    end
+
+    config.to_prepare do
+      ApplicationController.helper(NestedFieldsHelper)
+    end
+  end
+end
